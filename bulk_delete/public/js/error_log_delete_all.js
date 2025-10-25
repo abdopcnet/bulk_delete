f390728d06dd932c791f4e03edcdccd3590d1549
abdopcnet <abdopcnet@gmail.com>
2025-10-25 14:07:11 +0300

    Refactor delete all button to include confirmation prompt and improve button styling

--- bulk_delete/public/js/error_log_delete_all.js
+++ bulk_delete/public/js/error_log_delete_all.js
@@ -1,14 +1,12 @@
 frappe.listview_settings['Error Log'] = {
-    onload: function(listview) {
-        listview.page.add_inner_button('Delete All', function() {
-            frappe.confirm('Are you sure you want to delete all error logs?', function() {
-                frappe.call({
-                    method: 'frappe.core.doctype.error_log.error_log.clear_error_logs',
-                    callback: function() {
-                        listview.refresh();
-                    }
-                });
+    onload: function (listview) {
+        listview.page.add_inner_button(__('Delete All'), function () {
+            frappe.call({
+                method: 'frappe.core.doctype.error_log.error_log.clear_error_logs',
+                callback: function () {
+                    listview.refresh();
+                }
             });
-        });
+        }).addClass("btn-danger");
     }
 };
